8d836e3352ed99691615989a0e1eba81831aeced
Aayush Iyer <aayush.iyer@gmail.com>
2012-09-28 20:06:04 -0600

    Initial

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-tutorial-app
-============
+# Ruby on Rails Tutorial: sample application
 
-Rails tutorial app
+This is the sample application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutorialApp::Application.config.secret_token = 'c34274d8c9bbf597c468c0093935feee6e23539b36122b4bc70f3ecedec1e7962601a987bcbce1000b1c59d16dac91a5fd9b7bce4a167e620723bde8b0743a03'
